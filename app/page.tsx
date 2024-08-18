2a566eb9cee13c4f2aab3c8df83673fa264b406b
bonbon123123 <62888454+bonbon123123@users.noreply.github.com>
2024-08-18 22:30:20 +0200

    Changed imput form

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import { createUploadthing, type FileRouter } from "uploadthing/next";
 import ImageUpload from "./components/image_upload";
-
+import { DropImageUploader } from "./components/image_dropzone";
 const f = createUploadthing();
 
 
@@ -9,7 +9,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>Main</h1>
-      <ImageUpload />
+      <DropImageUploader />
     </main>
   );
 }
