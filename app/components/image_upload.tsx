a5b0cf5ba9720a13cf1850a0a38f37e7420db831
bonbon123123 <62888454+bonbon123123@users.noreply.github.com>
2024-08-18 19:29:17 +0200

    updating upload button

--- app/components/image_upload.tsx
+++ app/components/image_upload.tsx
@@ -1,31 +1,39 @@
 "use client";
 import { UploadButton } from "@uploadthing/react";
 import { OurFileRouter } from "../api/uploadthing/core";
+import { useState } from "react";
+import Image from "next/image";
 
 const ImageUpload = () => {
+    const [imageUrl, setImageUrl] = useState<string>("")
     return (
-        <UploadButton<OurFileRouter, "imageUploader">
-            endpoint="imageUploader"
-            onClientUploadComplete={(res) => {
-                // Do something with the response
-                console.log("Files: ", res);
-                alert("Upload Completed");
-            }}
-            onUploadError={(error: Error) => {
-                // Do something with the error.
-                alert(`ERROR! ${error.message}`);
-            }}
-            onBeforeUploadBegin={(files) => {
-                // Preprocess files before uploading (e.g. rename them)
-                return files.map(
-                    (f) => new File([f], "renamed-" + f.name, { type: f.type }),
-                );
-            }}
-            onUploadBegin={(name) => {
-                // Do something once upload begins
-                console.log("Uploading: ", name);
-            }}
-        />
+        <div>
+            <UploadButton<OurFileRouter, "imageUploader">
+                endpoint="imageUploader"
+                onClientUploadComplete={(res) => {
+                    setImageUrl(res[0].url)
+                }}
+                onUploadError={(error: Error) => {
+                    // Do something with the error.
+                    alert(`ERROR! ${error.message}`);
+                }}
+                onBeforeUploadBegin={(files) => {
+                    // Preprocess files before uploading (e.g. rename them)
+                    return files.map(
+                        (f) => new File([f], "renamed-" + f.name, { type: f.type }),
+                    );
+                }}
+                onUploadBegin={(name) => {
+                    // Do something once upload begins
+                    console.log("Uploading: ", name);
+                }}
+            />
+            {imageUrl.length ? (
+                <div>
+                    <Image src={imageUrl} alt="myImage" width={500} height={500} />
+                </div>
+            ) : null}
+        </div>
     );
 };
 
