8ef3a8a5bfef2ff2f4abfd9f7fb825bcc946b26c
bonbon123123 <62888454+bonbon123123@users.noreply.github.com>
2024-09-07 19:56:13 +0200

    Type error: File '/vercel/path0/app/api/sessionAnon/route.tsx' is not a module.

--- app/api/sessionAnon/route.tsx
+++ app/api/sessionAnon/route.tsx
@@ -1,17 +1,17 @@
-// import { NextApiRequest, NextApiResponse } from 'next';
+import { NextApiRequest, NextApiResponse } from 'next';
 
-// const authenticate = async (req: NextApiRequest, res: NextApiResponse) => {
-//     const { username, password } = req.body;
+export async function POST(req: Request) {
 
-//     // Tutaj musisz wstawić kod, który będzie sprawdzał dane logowania
-//     // Możesz korzystać z bazy danych lub innego mechanizmu uwierzytelniania
-//     const user = await sprawdzDaneLogowania(username, password);
 
-//     if (user) {
-//         res.status(200).json(user);
-//     } else {
-//         res.status(401).json({ error: 'Nieprawidłowe dane logowania' });
-//     }
-// };
+    // const { username, password } = req.body;
 
-// export default authenticate;
+    // // Tutaj musisz wstawić kod, który będzie sprawdzał dane logowania
+    // // Możesz korzystać z bazy danych lub innego mechanizmu uwierzytelniania
+    // const user = await sprawdzDaneLogowania(username, password);
+
+    // if (user) {
+    //     res.status(200).json(user);
+    // } else {
+    //     res.status(401).json({ error: 'Nieprawidłowe dane logowania' });
+    // }
+};
